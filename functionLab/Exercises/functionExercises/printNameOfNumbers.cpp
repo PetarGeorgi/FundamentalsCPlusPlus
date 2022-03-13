dc148c9cff69005acf210aaf2fe69d5022f15bab
Petar Valeriev Georgiev <penet@abv.bg>
2022-03-13 18:15:52 +0200

    Greatest Common Divisor

---
+++ functionLab/Exercises/functionExercises/printNameOfNumbers.cpp
@@ -0,0 +1,233 @@
+//Write a program that, given an integer number in the range [0, 9999], prints the name of that number in English.
+//Simplifications:
+// Use lowercase English letters only
+// Don’t place "and" (e.g. 957 is nine hundred fifty-seven, NOT nine hundred and fifty-seven)
+// Skip 0 digits, except for the number 0 (e.g. 0 -> zero; 101 -> one hundred one; 1001 -> one thousand one)
+// Don’t print dashes (e.g. print 75 as seventy-five, NOT seventy-five)
+
+#include <iostream>
+//#include "climits"
+//#include <cmath>
+using namespace std;
+void findTheThousandNumber (int x) { // thousandth
+    int result = x / 1000;
+    if(result > 0) {
+        switch (result) {
+            case 9 :
+                cout << "nine thousand ";
+                break;
+            case 8 :
+                cout << "eight thousand ";
+                break;
+            case 7 :
+                cout << "seven thousand ";
+                break;
+            case 6 :
+                cout << "six thousand ";
+                break;
+            case 5 :
+                cout << "five thousand ";
+                break;
+            case 4 :
+                cout << "four thousand ";
+                break;
+            case 3 :
+                cout << "three thousand ";
+                break;
+            case 2 :
+                cout << "two thousand ";
+                break;
+            case 1 :
+                cout << "thousand ";
+                break;
+
+        }
+    }else if(result == 0){ cout << "";}
+
+
+}
+
+void findTheHundredNumbers(int x) { //hundredth
+    x /= 100;
+    if (x > 0) {
+        switch (x) {
+            case 9 :
+                cout << "nine hundred ";
+                break;
+            case 8 :
+                cout << "eight hundred ";
+                break;
+            case 7 :
+                cout << "seven hundred ";
+                break;
+            case 6 :
+                cout << "six hundred ";
+                break;
+            case 5 :
+                cout << "five hundred ";
+                break;
+            case 4 :
+                cout << "four hundred ";
+                break;
+            case 3 :
+                cout << "three hundred ";
+                break;
+            case 2 :
+                cout << "two hundred ";
+                break;
+            case 1 :
+                cout << "one hundred ";
+                break;
+        }
+
+    } else if (x == 0) { cout << ""; }
+}
+ void findTheTensNumbers(int x) { //tenth
+        int memoryX = x;
+        x /= 10;
+     if (x >= 2 && x <= 9) {
+         switch (x) {
+             case 9 :
+                 cout << "ninety ";
+                 break;
+             case 8 :
+                 cout << "eighty ";
+                 break;
+             case 7 :
+                 cout << "seventy ";
+                 break;
+             case 6 :
+                 cout << "sixty ";
+                 break;
+             case 5 :
+                 cout << "fifty ";
+                 break;
+             case 4 :
+                 cout << "forty ";
+                 break;
+             case 3 :
+                 cout << "thirty ";
+                 break;
+             case 2 :
+                 cout << "twenty ";
+                 break;
+         }
+
+     } else if (x == 1) { // teen numbers
+         memoryX /= 10;
+         switch (memoryX) {
+             case 9 :
+                 cout << "nineteen";
+                 break;
+             case 8 :
+                 cout << "eighteen";
+                 break;
+             case 7 :
+                 cout << "seventeen";
+                 break;
+             case 6 :
+                 cout << "sixteen";
+                 break;
+             case 5 :
+                 cout << "fifteen";
+                 break;
+             case 4 :
+                 cout << "fourteen";
+                 break;
+             case 3 :
+                 cout << "thirteen";
+                 break;
+             case 2 :
+                 cout << "twelve";
+                 break;
+             case 1 :
+                 cout << "eleven";
+                 break;
+             default:
+                 cout << "ten";
+                 break;
+
+         }
+     }
+ }
+     void findTheSmallNumbers(int x) { //digits
+         switch (x) {
+             case 9 :
+                 cout << "nine";
+                 break;
+             case 8 :
+                 cout << "eight";
+                 break;
+             case 7 :
+                 cout << "seven";
+                 break;
+             case 6 :
+                 cout << "six";
+                 break;
+             case 5 :
+                 cout << "five";
+                 break;
+             case 4 :
+                 cout << "four";
+                 break;
+             case 3 :
+                 cout << "three";
+                 break;
+             case 2 :
+                 cout << " two";
+                 break;
+             case 1 :
+                 cout << "one";
+                 break;
+             case 0:
+                 cout << "zero";
+                 break;
+         }
+
+     }
+
+//void print_each_digit(int x)
+//{
+//
+//    if(x >= 10)
+//        n++;
+//        print_each_digit(x / 10);
+//
+//    int digit = x % 10;
+//
+//    std::cout << digit << '\n';
+//}
+int main() {
+    int x;
+    cin >> x;
+    if(x == 0) {
+        cout << "zero";
+        return 0;
+    }
+    findTheThousandNumber(x);
+    x %= 1000;
+    if(x == 0) {
+         return 0;
+    }
+    findTheHundredNumbers(x);
+    x %= 100;
+    if( x == 10){
+        cout << "ten";
+        return 0;
+    }
+    if(x == 0) {
+        return 0;
+    }
+    findTheTensNumbers(x);
+
+    if(x == 10 || x == 20 || x == 30 || x == 40 || x == 50 || x == 60 || x == 70 || x == 80 || x == 90 ){
+        return 0;
+    }
+    if(x >= 11 && x <= 19){
+        return 0;
+    }
+    x %= 10;
+    findTheSmallNumbers(x);
+
+    return 0;
+}
